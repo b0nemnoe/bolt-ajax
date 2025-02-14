954b897e93e5f8a70d8cb9fb7e2926fede9ebeab
b0nemnoe <b0nemnoe@vasvari.org>
2025-02-14 11:17:03 +0100

    first commit

---
+++ bolt-ajax/src/stores/bot.js
@@ -0,0 +1,54 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+import axios from 'axios'
+import { useToast } from 'vue-toastification'
+
+export const useBotStore = defineStore('bot', () => {
+  const products = ref([])
+  const cart = ref({})  
+  const toast = useToast()
+  
+  const loadAll = () => {
+    fetch("http://localhost:3000/bolt")
+    .then(resp => resp.json())
+    .then(data => products.value = data)
+  }
+
+  const addToCart = (id) =>{    
+    cart.value[id] = cart.value[id] ? cart.value[id] + 1 : 1
+    toast("Kosárhoz adva")
+    products.value.find((p) => p.id == id).store -= 1
+  }
+
+  const saveProduct = (p) => {
+    console.log(p)
+    //let id = Math.round(Math.random() * 1000000000)
+    products.value.push(p)
+    axios.post("http://localhost:3000/bolt",p)
+    .then(resp => {
+      console.log(resp.statusText)
+      toast("Sikeres mentés")
+    })
+    .catch(() => toast.error("Hiba"))
+  }
+
+  const emptyCart = () => {
+    for (const key in cart.value) {
+      products.value.find((p) => p.id == key).store += cart.value[key]
+    }
+    cart.value = {};
+    toast.error("Kosár kiürítve")
+  }
+
+  const countTotal = () => {
+    let total = 0
+    for (const i in cart.value) {
+      total += cart.value[i] * parseFloat(products.value.find(p => p.id == i).price)
+    }
+    return total
+  }
+
+
+
+  return { products , cart, loadAll, addToCart, saveProduct, emptyCart, countTotal }
+})
---
+++ bolt-ajax/src/views/CartView.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1 class="text-center">Kosár</h1>
+    <p v-if="Object.entries(botStore.cart).length < 1"> Üres a kosár!</p>
+    <div v-else>
+      <table class="table table-striped col-md-9">
+        <thead>
+          <tr>
+            <th>Termék</th>
+            <th>Ár</th>
+            <th>Mennyiség</th>
+            <th>Összesen</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(v, k) in botStore.cart">
+            <td>{{ botStore.products.find(p => p.id === k).name }}</td>
+            <td>{{ botStore.products.find(p => p.id === k).price }} </td>
+            <td>{{ v }}</td>
+            <td>{{ v * parseFloat(botStore.products.find(p => p.id === k).price) }} Ft</td>
+          </tr>
+        </tbody>
+        <tfooter>
+          <tr class="fs-5">
+            <td colspan="3">Végösszeg: </td>
+            <td>{{ botStore.countTotal() }} Ft</td>
+          </tr>
+        </tfooter>
+      </table>
+      <button @click="botStore.emptyCart()" class="btn btn-outline-danger">Kosár kiürítése</button>
+    </div>
+</template>
+
+<script setup>
+  import {useBotStore} from '@/stores/bot.js'
+  const botStore = useBotStore()
+
+
+
+</script>
