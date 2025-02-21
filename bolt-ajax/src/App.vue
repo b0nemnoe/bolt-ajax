83beb2c0ac94f3c64da49b6b252b10df3eda2680
b0nemnoe <b0nemnoe@vasvari.org>
2025-02-21 08:56:48 +0100

    localstorage és jelzi hogy hány termék van a kosárba

--- bolt-ajax/src/App.vue
+++ bolt-ajax/src/App.vue
@@ -1,23 +1,29 @@
 <script setup>
-import {onMounted} from 'vue'
+import { onMounted, computed } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
 import { useBotStore } from '@/stores/bot.js'
 
 const botStore = useBotStore()
 
-onMounted(() =>{
+onMounted(() => {
   botStore.loadAll()
 })
 
+// 🟢 Kosárban lévő termékek számának kiszámítása
+const cartItemCount = computed(() => {
+  return Object.values(botStore.cart).reduce((acc, qty) => acc + qty, 0)
+})
 </script>
 
 <template>
-      <div class="container">
-        <nav class="text-center">
-          <RouterLink class="btn btn-outline-success m-2" to="/">Termékek</RouterLink>
-          <RouterLink class="btn btn-outline-success m-2" to="/new">Új termék</RouterLink>
-          <RouterLink  class="btn btn-outline-success m-2" to="/cart">Kosár</RouterLink>
-        </nav>
-          <RouterView />
-      </div>
+  <div class="container">
+    <nav class="text-center">
+      <RouterLink class="btn btn-outline-success m-2" to="/">Termékek</RouterLink>
+      <RouterLink class="btn btn-outline-success m-2" to="/new">Új termék</RouterLink>
+      <RouterLink class="btn btn-outline-success m-2" to="/cart">
+        Kosár <span v-if="cartItemCount > 0">({{ cartItemCount }})</span>
+      </RouterLink>
+    </nav>
+    <RouterView />
+  </div>
 </template>
--- bolt-ajax/src/stores/bot.js
+++ bolt-ajax/src/stores/bot.js
@@ -1,4 +1,4 @@
-import { ref, computed } from "vue"
+import { ref, computed, onMounted, watch } from "vue"
 import { defineStore } from "pinia"
 import axios from "axios"
 import { useToast } from "vue-toastification"
@@ -8,25 +8,62 @@ export const useBotStore = defineStore("bot", () => {
   const cart = ref({})
   const toast = useToast()
 
-  const loadAll = () => {
-    fetch("http://localhost:3000/bolt")
-      .then((resp) => resp.json())
-      .then((data) => (products.value = data))
+  const loadAll = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/bolt");
+      products.value = await response.json();
+
+      const savedCart = localStorage.getItem("cart");
+      if (savedCart) {
+        cart.value = JSON.parse(savedCart);
+      }
+
+      for (const id in cart.value) {
+        const product = products.value.find((p) => p.id == id);
+        if (product) {
+          product.store -= cart.value[id];
+        }
+      }
+    } catch (error) {
+      toast.error("Nem sikerült betölteni a termékeket!");
+    }
   };
+
+  onMounted(() => {
+    const savedCart = localStorage.getItem("cart")
+    if (savedCart) {
+      cart.value = JSON.parse(savedCart)
+    }
+    loadAll() 
+  })
+
+  watch(cart, (newCart) => {
+    localStorage.setItem("cart", JSON.stringify(newCart))
+  }, { deep: true })
 
   const addToCart = (id) => {
-    if (products.value.find((p) => p.id == id).store == 0) {
-      toast.error("Nincs készleten")
+    const product = products.value.find((p) => p.id == id);
+  
+    if (!product) {
+      toast.error("A termék nem elérhető!");
       return;
     }
-    cart.value[id] = cart.value[id] ? cart.value[id] + 1 : 1
-    toast("Kosárhoz adva")
-    products.value.find((p) => p.id == id).store -= 1
+  
+    if (product.store === 0) {
+      toast.error("Nincs készleten");
+      return;
+    }
+  
+    cart.value[id] = cart.value[id] ? cart.value[id] + 1 : 1;
+    product.store--; 
+  
+    localStorage.setItem("cart", JSON.stringify(cart.value));
+    toast("Kosárhoz adva");
   };
-
+  
   const saveProduct = (p) => {
     console.log(p);
-    //let id = Math.round(Math.random() * 1000000000)
+
     products.value.push(p)
     axios
       .post("http://localhost:3000/bolt", p)
@@ -55,25 +92,42 @@ export const useBotStore = defineStore("bot", () => {
   }
 
   const deleteProduct = (id) => {
-    products.value.find((p) => p.id == id).store += cart.value[id]
-    delete cart.value[id]
-    toast.error("Termék törölve a kosárból!")
+    const product = products.value.find((p) => p.id == id);
+    if (product) {
+      product.store += cart.value[id];
+    }
+    delete cart.value[id];
+    toast.error("Termék törölve a kosárból!");
   }
 
   const modifyQuantity = (id, op) => {
-    if (op == '+') {
-      addToCart(id)
-    } else {
-      if (cart.value[id] == 1) {
-        deleteProduct(id)
-      } else {
-        cart.value[id] --
-        products.value.find((p) => p.id == id).store ++
-        toast.warning("Mennyiség csökkentve")
-      }
-    }
-  }
+    if (!cart.value[id]) return; 
+  
+    const product = products.value.find((p) => p.id == id);
+    if (!product) return;
 
+    if (op === "+") {
+      if (cart.value[id] < product.store + cart.value[id]) {
+        cart.value[id]++;
+        product.store--;
+        toast("Mennyiség növelve");
+      } else {
+        toast.error("Nincs több készleten!");
+      }
+    } else {
+      if (cart.value[id] === 1) {
+        delete cart.value[id];
+        toast.error("Termék eltávolítva a kosárból!");
+      } else {
+        cart.value[id]--;
+        toast.warning("Mennyiség csökkentve");
+      }
+      product.store++;
+    }
+  
+    localStorage.setItem("cart", JSON.stringify(cart.value));
+  };
+  
   return {
     products,
     cart,
