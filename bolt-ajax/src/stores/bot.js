10c7edc3f307a0050213d977e63f828603bb8bcb
b0nemnoe <b0nemnoe@vasvari.org>
2025-11-30 20:03:21 +0100

    computed importalva

--- bolt-ajax/src/stores/bot.js
+++ bolt-ajax/src/stores/bot.js
@@ -1,4 +1,4 @@
-import { ref, watch, onMounted } from "vue"
+import { ref, watch, onMounted, computed } from "vue"
 import { defineStore } from "pinia"
 import axios from "axios"
 import { useToast } from "vue-toastification"
